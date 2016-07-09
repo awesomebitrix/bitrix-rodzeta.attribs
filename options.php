7162da45cec58b7ee6c2e813490469ec90d84fa7
Rick Deckard <xxxblasteroid@gmail.com>
2016-07-09 18:05:44 +0300

    fix

--- options.php
+++ options.php
@@ -77,6 +77,10 @@ $tabControl->begin();
 
 	<?php $tabControl->beginNextTab() ?>
 
+	<tr class="heading">
+		<td colspan="2">Настройки для фильтра по атрибутам</td>
+	</tr>
+
 	<tr>
 		<td class="adm-detail-content-cell-l" width="50%">
 			<label>ID инфоблока "Контент"</label>
