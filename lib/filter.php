9055027b944fe76ac8e361a3aa0d5edb5acb5fab
Rick Deckard <xxxblasteroid@gmail.com>
2016-07-09 18:00:26 +0300

    fix

---
+++ lib/filter.php
@@ -0,0 +1,14 @@
+<?php
+/***********************************************************************************************
+ * rodzeta.attribs - Infoblock element attributes
+ * Copyright 2016 Semenov Roman
+ * MIT License
+ ************************************************************************************************/
+
+namespace Rodzeta\Attribs;
+
+final class Filter {
+
+	//...
+
+}
