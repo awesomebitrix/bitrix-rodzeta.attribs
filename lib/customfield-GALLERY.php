65cadfbf5f72e1d25f0660b469e86e5c3ef34864
Rick Deckard <xxxblasteroid@gmail.com>
2016-10-24 14:18:25 +0300

    fix

--- lib/customfield-GALLERY.php
+++ lib/customfield-GALLERY.php
@@ -1,60 +1,28 @@
 <?php
 
-if (\CModule::IncludeModule("fileman")) {
-  \CMedialib::Init();
+namespace Rodzeta\Attribs;
+
+\CModule::IncludeModule("fileman");
+\CMedialib::Init();
+
+$arCol = \CMedialibCollection::GetList(array(
+  "arFilter" => array("ACTIVE" => "Y"),
+  "arOrder" => array("NAME" => "ASC")
+));
+$galleryCollections = array();
+foreach ($arCol as $item) {
+  $galleryCollections[$item["ID"]] = $item;
 }
 
-return;
-
-$parentId = null;
-if (!empty($value["VALUE"])) {
-  // get parent id
-  foreach (\CMedialibCollection::GetList(array(
-        "arFilter" => array(
-          "ID" => $value["VALUE"],
-          "ACTIVE" => "Y"
-        ),
-        "arOrder" => array("NAME" => "ASC")
-      )) as $gallery) {
-    print_r($gallery);
-  }
-  //$current = \rivetweb\entity\Media::selectSection([])->current();
-  //$parentId = $current["PARENT_ID"];
-}
+$galleryCollections = Utils::buildTree($galleryCollections);
+$optionsGallery = array(0 => " --- ");
+Utils::printTree($galleryCollections, $optionsGallery);
 
 ?>
 
-<select class="form-input-gallery-img-parent">
-  <option value="">- Выберите категорию -</option>
-
-  <?php foreach (\rivetweb\entity\Media::selectSection(["PARENT_ID" => 1]) as $parent) { ?>
-    <option <?= $parentId == $parent["ID"]? "selected" : "" ?> value="<?= $parent["ID"] ?>"><?= $parent["NAME"] ?></option>
-  <?php } ?>
-
-</select>
-
 <select name="<?= $strHTMLControlName["VALUE"] ?>" class="form-input-gallery-img">
-  <option value="">- Выберите галерею -</option>
-
-  <?php if (!empty($parentId)) { ?>
-    <?php foreach (\rivetweb\entity\Media::selectSection(["PARENT_ID" => $parentId]) as $item) { ?>
-      <option <?= $value["VALUE"] == $item["ID"]? "selected" : "" ?> value="<?= $item["ID"] ?>"><?= $item["NAME"] ?></option>
-    <?php } ?>
+  <?php foreach ($optionsGallery as $optionValue => $optionName) { ?>
+      <option <?= $value["VALUE"] == $optionValue? "selected" : "" ?>
+        value="<?= $optionValue ?>"><?= $optionName ?></option>
   <?php } ?>
-
 </select>
-
-<script>
-  jQuery(function ($) {
-    $(".form-input-gallery-img-parent").change(function () {
-      $.post("/api/media/options/?id=" + $(this).val()).then(function (data) {
-        var options = JSON.parse(data);
-        var result = ["<option value=\"\">- Выберите галерею -</option>"];
-        for (var i in options) {
-          result.push("<option value=\"" + options[i].ID + "\">" + options[i].NAME + "</option>");
-        }
-        $(".form-input-gallery-img").html(result.join("\n"));
-      });
-    });
-  });
-</script>
--- lib/utils.php
+++ lib/utils.php
@@ -148,4 +148,26 @@ final class Utils {
 		return include $_SERVER["DOCUMENT_ROOT"] . self::MAP_NAME;
 	}
 
+	static function buildTree(&$elements, $parentId = 0) {
+		$branch = array();
+		foreach ($elements as &$element) {
+			if ($element["PARENT_ID"] == $parentId) {
+				$children = self::buildTree($elements, $element["ID"]);
+				if ($children) {
+					$element["CHILDREN"] = $children;
+				}
+				$branch[$element["ID"]] = $element;
+				unset($element);
+			}
+		}
+		return $branch;
+	}
+
+	static function printTree($elements, &$result, $level = 0) {
+		foreach ($elements as $element) {
+			$result[$element["ID"]] = str_repeat(" -", $level) . " " . $element["NAME"];
+			self::printTree($element["CHILDREN"], $result, $level + 1);
+		}
+	}
+
 }
