7d82771a1f8c3f8b9ff579f6140e1e5fef748f72
Rick Deckard <xxxblasteroid@gmail.com>
2016-10-24 11:47:56 +0300

    fix

--- .dev/attribs/default.php
+++
@@ -1,9 +0,0 @@
-
-<?php if ($field["ROWS"] > 1) { ?>
-  <textarea name="<?= $strHTMLControlName["VALUE"] ?>"
-    cols="<?= !empty($field["COLS"])? $field["COLS"] : $arProperty["COL_COUNT"] ?>"
-    rows="<?= $field["ROWS"] ?>"><?= htmlspecialcharsex($value["VALUE"])?></textarea>
-<?php } else { ?>
-  <input name="<?= $strHTMLControlName["VALUE"] ?>" value="<?= htmlspecialcharsex($value["VALUE"]) ?>"
-    size="<?= !empty($field["COLS"])? $field["COLS"] : $arProperty["COL_COUNT"] ?>" type="text">
-<?php } ?>
