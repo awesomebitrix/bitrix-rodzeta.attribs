9b43509650190b1bdcd542da845a7095679f557f
Rick Deckard <xxxblasteroid@gmail.com>
2016-11-01 08:09:26 +0300

    fix

---
+++ .init.php
@@ -0,0 +1,15 @@
+<?php
+/***********************************************************************************************
+ * rodzeta.attribs - Infoblock element attributes
+ * Copyright 2016 Semenov Roman
+ * MIT License
+ ************************************************************************************************/
+
+namespace Rodzeta\Attribs;
+
+define(__NAMESPACE__ . "\_APP", __DIR__ . "/");
+define(__NAMESPACE__ . "\_LIB", __DIR__  . "/lib/");
+define(__NAMESPACE__ . "\_FILE_ATTRIBS", "/upload/.rodzeta.attribs.php");
+define(__NAMESPACE__ . "\_FILE_ATTRIBS_CSV", "/upload/.rodzeta.attribs.csv");
+
+require _LIB . "encoding/csv.php";
