ca074be054a576e67dd9a9ad7a8020b114d881b9
Rick Deckard <xxxblasteroid@gmail.com>
2016-11-03 09:22:11 +0300

    fix-remove-csv

---
+++ install/data/.rodzeta.attribs.php
@@ -0,0 +1,196 @@
+<?php
+return array (
+  0 => 
+  array (
+    'PRICE' => 
+    array (
+      'CODE' => 'PRICE',
+      'ALIAS' => 'price',
+      'NAME' => 'Цена',
+      'HINT' => 'руб.',
+      'SECTIONS' => 
+      array (
+        39 => 0,
+        97 => 1,
+        175 => 2,
+      ),
+      'NUMERIC' => '1',
+      'FILTER' => '1',
+      'COMPARE' => '',
+      'SORT' => 100,
+      'INPUT_TYPE' => '',
+      'COLS' => '20',
+      'ROWS' => '',
+    ),
+    'PRICE_ORIGINAL' => 
+    array (
+      'CODE' => 'PRICE_ORIGINAL',
+      'ALIAS' => '',
+      'NAME' => 'Цена*',
+      'HINT' => 'исходная',
+      'SECTIONS' => 
+      array (
+        97 => 0,
+        99 => 1,
+      ),
+      'NUMERIC' => '1',
+      'FILTER' => '',
+      'COMPARE' => '',
+      'SORT' => 200,
+      'INPUT_TYPE' => '',
+      'COLS' => '20',
+      'ROWS' => '',
+    ),
+    'PRICE_OLD' => 
+    array (
+      'CODE' => 'PRICE_OLD',
+      'ALIAS' => '',
+      'NAME' => 'Старая цена',
+      'HINT' => 'руб.',
+      'SECTIONS' => '',
+      'NUMERIC' => '1',
+      'FILTER' => '',
+      'COMPARE' => '',
+      'SORT' => 300,
+      'INPUT_TYPE' => '',
+      'COLS' => '20',
+      'ROWS' => '',
+    ),
+    'AREA' => 
+    array (
+      'CODE' => 'AREA',
+      'ALIAS' => 'area',
+      'NAME' => 'Площадь',
+      'HINT' => 'кв.м',
+      'SECTIONS' => 
+      array (
+        39 => 0,
+        97 => 1,
+      ),
+      'NUMERIC' => '1',
+      'FILTER' => '1',
+      'COMPARE' => '',
+      'SORT' => 400,
+      'INPUT_TYPE' => '',
+      'COLS' => '',
+      'ROWS' => '',
+    ),
+    'AREA_SOT' => 
+    array (
+      'CODE' => 'AREA_SOT',
+      'ALIAS' => 'area_sot',
+      'NAME' => 'Площадь',
+      'HINT' => 'сотки',
+      'SECTIONS' => 
+      array (
+        39 => 0,
+        97 => 1,
+      ),
+      'NUMERIC' => '1',
+      'FILTER' => '1',
+      'COMPARE' => '',
+      'SORT' => 500,
+      'INPUT_TYPE' => '',
+      'COLS' => '',
+      'ROWS' => '',
+    ),
+    'ARTICLE' => 
+    array (
+      'CODE' => 'ARTICLE',
+      'ALIAS' => '',
+      'NAME' => 'Артикул',
+      'HINT' => '',
+      'SECTIONS' => 
+      array (
+        39 => 0,
+        97 => 1,
+      ),
+      'NUMERIC' => '',
+      'FILTER' => '',
+      'COMPARE' => '',
+      'SORT' => 600,
+      'INPUT_TYPE' => '',
+      'COLS' => '',
+      'ROWS' => '',
+    ),
+    'GALLERY' => 
+    array (
+      'CODE' => 'GALLERY',
+      'ALIAS' => '',
+      'NAME' => 'Галлерея изображений',
+      'HINT' => '',
+      'SECTIONS' => 
+      array (
+        39 => 0,
+        97 => 1,
+        138 => 2,
+      ),
+      'NUMERIC' => '',
+      'FILTER' => '',
+      'COMPARE' => '',
+      'SORT' => 5000,
+      'INPUT_TYPE' => 'GALLERY',
+      'COLS' => '',
+      'ROWS' => '',
+    ),
+    'FEATURES' => 
+    array (
+      'CODE' => 'FEATURES',
+      'ALIAS' => '',
+      'NAME' => 'Основные преимущества',
+      'HINT' => '',
+      'SECTIONS' => 
+      array (
+        39 => 0,
+        97 => 1,
+      ),
+      'NUMERIC' => '',
+      'FILTER' => '',
+      'COMPARE' => '',
+      'SORT' => 6000,
+      'INPUT_TYPE' => 'HTML',
+      'COLS' => '400',
+      'ROWS' => '200',
+    ),
+    'ADDRESS' => 
+    array (
+      'CODE' => 'ADDRESS',
+      'ALIAS' => '',
+      'NAME' => 'Адрес',
+      'HINT' => '',
+      'SECTIONS' => 
+      array (
+        39 => 0,
+        97 => 1,
+      ),
+      'NUMERIC' => '',
+      'FILTER' => '',
+      'COMPARE' => '',
+      'SORT' => 7000,
+      'INPUT_TYPE' => '',
+      'COLS' => '',
+      'ROWS' => '',
+    ),
+    'PHONE' => 
+    array (
+      'CODE' => 'PHONE',
+      'ALIAS' => '',
+      'NAME' => 'Номер телефона',
+      'HINT' => '',
+      'SECTIONS' => '',
+      'NUMERIC' => '',
+      'FILTER' => '',
+      'COMPARE' => '',
+      'SORT' => 7100,
+      'INPUT_TYPE' => '',
+      'COLS' => '',
+      'ROWS' => '',
+    ),
+  ),
+  1 => 
+  array (
+    'price' => 'price',
+    'area' => 'area',
+    'area_sot' => 'area_sot',
+  ),
+);
